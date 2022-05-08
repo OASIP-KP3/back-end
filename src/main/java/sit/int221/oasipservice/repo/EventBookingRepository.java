47fe675f318e69967db1991e924fb76da0b5c363
tan <t.songglod@gmail.com>
2022-05-08 09:41:22 +0700

    [+] update POST method: check for overlap

--- src/main/java/sit/int221/oasipservice/repo/EventBookingRepository.java
+++ src/main/java/sit/int221/oasipservice/repo/EventBookingRepository.java
@@ -1,7 +1,12 @@
 package sit.int221.oasipservice.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sit.int221.oasipservice.entities.EventBooking;
 
+import java.util.List;
+
 public interface EventBookingRepository extends JpaRepository<EventBooking, Integer> {
+    @Query(value = "SELECT * FROM event_booking WHERE date(event_start_time) = ?1 AND event_category = ?2", nativeQuery = true)
+    List<EventBooking> findAllByDateAndCategory(String date, Integer categoryId);
 }
--- src/main/java/sit/int221/oasipservice/services/EventBookingService.java
+++ src/main/java/sit/int221/oasipservice/services/EventBookingService.java
@@ -13,6 +13,11 @@ import sit.int221.oasipservice.entities.EventBooking;
 import sit.int221.oasipservice.repo.EventBookingRepository;
 import sit.int221.oasipservice.utils.ListMapper;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -35,13 +40,50 @@ public class EventBookingService {
     }
 
     public EventDetailsBaseDto getEventDetails(Integer id) {
-        EventBooking booking = repo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        EventBooking booking = repo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, id + " does not exist"));
         return modelMapper.map(booking, EventDetailsBaseDto.class);
     }
 
     public void save(EventBookingDto newBooking) {
+        if (isOverlap(newBooking)) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, newBooking.getEventStartTime() + " is overlap");
+        } else {
+            EventBooking booking = modelMapper.map(newBooking, EventBooking.class);
+            repo.saveAndFlush(booking);
+        }
+    }
+
+    private boolean isOverlap(EventBookingDto newBooking) {
         EventBooking booking = modelMapper.map(newBooking, EventBooking.class);
-        repo.saveAndFlush(booking);
+
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDate date = LocalDate.ofInstant(booking.getEventStartTime(), zone);
+        Integer categoryId = booking.getEventCategory().getId();
+        List<EventBooking> bookings = repo.findAllByDateAndCategory(date.toString(), categoryId);
+
+        LocalTime startA = getStartTime(booking, zone);
+        LocalTime endA = getEndTime(booking, zone);
+
+        for (EventBooking book : bookings) {
+            LocalTime startB = getStartTime(book, zone);
+            LocalTime endB = getEndTime(book, zone);
+
+            if (startA.isBefore(endB) && endA.isAfter(startB)) return true;
+        }
+        return false;
+    }
+
+    private Instant plusMinutes(Instant dateTime, long minutes) {
+        return dateTime.plus(minutes, ChronoUnit.MINUTES);
+    }
+
+    private LocalTime getStartTime(EventBooking booking, ZoneId zone) {
+        return LocalTime.ofInstant(booking.getEventStartTime(), zone);
+    }
+
+    private LocalTime getEndTime(EventBooking booking, ZoneId zone) {
+        long minutes = booking.getEventDuration().longValue();
+        return LocalTime.ofInstant(plusMinutes(booking.getEventStartTime(), minutes), zone);
     }
 
     public void delete(Integer id) {
