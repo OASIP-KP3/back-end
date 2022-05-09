86e00301234ffcab5361ffae5c209564827d644c
tan <t.songglod@gmail.com>
2022-05-09 15:04:51 +0700

    [+] update set method

--- src/main/java/sit/int221/oasipservice/dto/events/EventBookingDto.java
+++ src/main/java/sit/int221/oasipservice/dto/events/EventBookingDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.time.ZoneId;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,4 +20,9 @@ public class EventBookingDto {
     private Instant eventStartTime;
     private Integer eventDuration;
     private String eventNotes;
+
+    public EventBookingDto setEventStartTime(Instant eventStartTime) {
+        this.eventStartTime = eventStartTime.atZone(ZoneId.systemDefault()).toInstant();
+        return this;
+    }
 }
