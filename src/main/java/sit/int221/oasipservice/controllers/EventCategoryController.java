92a22cbc50fd42efeeb042f272cf9d3076595fa9
tan <t.songglod@gmail.com>
2022-05-20 02:00:26 +0700

    [+] minor fixes

--- src/main/java/sit/int221/oasipservice/controllers/EventCategoryController.java
+++ src/main/java/sit/int221/oasipservice/controllers/EventCategoryController.java
@@ -1,13 +1,15 @@
 package sit.int221.oasipservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sit.int221.oasipservice.dto.categories.CategoryDto;
 import sit.int221.oasipservice.services.EventCategoryService;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/categories")
 public class EventCategoryController {
