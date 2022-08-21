f888bceb41b0534b358cf1cb12c7f46007a2af12
tan <t.songglod@gmail.com>
2022-08-21 23:02:06 +0700

    [+] change data type

--- src/main/java/sit/int221/oasipservice/dto/users/UserDetailsDto.java
+++ src/main/java/sit/int221/oasipservice/dto/users/UserDetailsDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @AllArgsConstructor
@@ -16,6 +17,6 @@ public class UserDetailsDto {
     private String userName;
     private String userEmail;
     private String userRole;
-    private OffsetDateTime createdOn;
-    private OffsetDateTime updatedOn;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
 }
