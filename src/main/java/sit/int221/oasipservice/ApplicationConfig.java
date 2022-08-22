5edbbd613d050e21cda3084710ff744fb75fefc3
tan <t.songglod@gmail.com>
2022-08-22 12:43:07 +0700

    [+] test new datetime annotation

--- src/main/java/sit/int221/oasipservice/ApplicationConfig.java
+++ src/main/java/sit/int221/oasipservice/ApplicationConfig.java
@@ -3,11 +3,9 @@ package sit.int221.oasipservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import sit.int221.oasipservice.utils.ListMapper;
 
 @Configuration
-@EnableJpaAuditing
 public class ApplicationConfig {
     @Bean
     public ListMapper listMapper() {
--- src/main/java/sit/int221/oasipservice/entities/User.java
+++ src/main/java/sit/int221/oasipservice/entities/User.java
@@ -2,9 +2,8 @@ package sit.int221.oasipservice.entities;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.OffsetDateTime;
@@ -12,7 +11,6 @@ import java.time.OffsetDateTime;
 @Getter
 @Setter
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user", indexes = {
         @Index(name = "PRIMARY", columnList = "user_id"),
         @Index(name = "user_name_UNIQUE", columnList = "user_name"),
@@ -36,11 +34,11 @@ public class User {
     @Column(name = "user_role", nullable = false)
     private String userRole;
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(name = "createdOn", nullable = false)
     private OffsetDateTime createdOn;
 
-    @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "updatedOn", nullable = false)
     private OffsetDateTime updatedOn;
 }
